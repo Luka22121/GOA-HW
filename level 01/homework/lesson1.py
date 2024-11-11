6903daab9dadc55ac5ca884b37391fd38b361dd2
Luka22121 <senidzmakaciluka@gmail.com>
2024-11-11 19:56:06 +0400

    first commit

---
+++ level 01/homework/lesson1.py
@@ -0,0 +1,18 @@
+name = "luka"
+surname = "sexniashvili"
+age = 14
+height = 176.2
+print (name)
+print (name  +" "+ surname )
+knows_programming = True
+its_ugyl = False
+print(type(age))
+print(type(name))
+print(type(surname))
+print(type(height))
+print(type(knows_programming))
+
+print(name + " "+  str (age))
+
+ 
+
